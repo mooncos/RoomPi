mooncos/RoomPi

commit 23d95b1265deec31a558e70c4a8ee59726ff74a0

Add CO2 flags to buzzer and LED fsms

src/controllers/outputctrl.c
@@ -213,12 +213,12 @@ static int _next_display_warning(fsm_t *this) {
 }
 
 static int _general_anomaly(fsm_t *this) {
-	int res = measurement_flags & (FLAG_TEMP_ANOMALY | FLAG_HUMID_ANOMALY | FLAG_LIGHT_ANOMALY);
+	int res = measurement_flags & (FLAG_TEMP_ANOMALY | FLAG_HUMID_ANOMALY | FLAG_LIGHT_ANOMALY | FLAG_CO2_EMERGENCY);
 	return res;
 }
 
 static int _general_emergency(fsm_t *this) {
-	int res = measurement_flags & (FLAG_TEMP_EMERGENCY | FLAG_HUMID_EMERGENCY | FLAG_LIGHT_EMERGENCY);
+	int res = measurement_flags & (FLAG_TEMP_EMERGENCY | FLAG_HUMID_EMERGENCY | FLAG_LIGHT_EMERGENCY | FLAG_CO2_EMERGENCY);
 	return res;
 }
 

commit 6facb7c4b426467e9cc865b37814a81d51b31ab9

fix anomaly flag bug on LED and Buzzer fsms introduced by 23d95b1265deec31a558e70c4a8ee59726ff74a0

src/controllers/outputctrl.c
@@ -213,7 +213,7 @@ static int _next_display_warning(fsm_t *this) {
 }
 
 static int _general_anomaly(fsm_t *this) {
-	int res = measurement_flags & (FLAG_TEMP_ANOMALY | FLAG_HUMID_ANOMALY | FLAG_LIGHT_ANOMALY | FLAG_CO2_EMERGENCY);
+	int res = measurement_flags & (FLAG_TEMP_ANOMALY | FLAG_HUMID_ANOMALY | FLAG_LIGHT_ANOMALY | FLAG_CO2_ANOMALY);
 	return res;
 }
 